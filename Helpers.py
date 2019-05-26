a307173b8e5826c6fabee19c2cf4cebf548f8710
mlo31415 <mlo@baskerville.org>
2019-05-25 21:59:36 -0400

    Add the Helpers file

---
+++ Helpers.py
@@ -0,0 +1,16 @@
+import re
+
+#*******************************************************
+# Is this likely to be a person's name?
+# A hit is of the form <name1> <initial> <name2> where name1 is in the list of first names
+def IsAName(s: str):
+    pattern="^([A-Z]([a-z]|+\.)\s+([A-Z]\.?)\s+([A-Z]([a-z]|+\.)$"
+    m=re.match(pattern, s.strip())
+    if m is None:
+        return False
+
+    firstnames=["Bob", "Robert", "Don", "Donald", "Alice"]
+    if m.groups()[0] in firstnames:
+        return True
+
+    return False
